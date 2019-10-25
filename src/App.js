8907c8b1e28f202d4e55f400e8a02554c11010fe
Murat Kemaldar <murat.kemaldar@outlook.com>
2019-10-25 18:31:12 +0200

    axes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear
+} from "d3";
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
@@ -9,27 +16,55 @@ function App() {
   // will be called initially and on every data change
   useEffect(() => {
     const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat(index => index + 1);
+    svg
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
 
     // generates the "d" attribute of a path element
     const myLine = line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(curveCardinal);
 
     // renders path element, and attaches
     // the "d" attribute from line generator above
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
-      .attr("d", value => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "blue");
   }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <br />
+      <br />
+      <br />
       <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
