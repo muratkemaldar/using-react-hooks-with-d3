d1a0b96b08a76940ec6f3635ea4802f47540765e
Murat Kemaldar <murat.kemaldar@outlook.com>
2019-11-03 01:44:42 +0100

    responsive

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Using React (Hooks) with D3 - [05] Interactivity
+# Using React (Hooks) with D3 - [06] Responsive Chart Components with ResizeObserver
 
 This is the code of the video tutorial series "Using React (Hooks) with D3".
 
 Each tutorial session is saved within a branch,
 so if you wanna work with the code from this repo, checkout the branches!
 
-This session is about creating a bar chart, which is gonna be colored and animated!
+This session is about adding responsiveness with the new ResizeObserver API!
 
 ## Getting Started
 
 `git clone git@github.com:muratkemaldar/using-react-hooks-with-d3.git`
 
-`git checkout 05-interactivity-basic`
+`git checkout 06-components-responsive`
 
 `npm install`
 
--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
+import BarChart from "./BarChart";
 import "./App.css";
-import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
-  const svgRef = useRef();
-
-  // will be called initially and on every data change
-  useEffect(() => {
-    const svg = select(svgRef.current);
-
-    // scales
-    const xScale = scaleBand()
-      .domain(data.map((value, index) => index))
-      .range([0, 300])
-      .padding(0.5);
-
-    const yScale = scaleLinear()
-      .domain([0, 150])
-      .range([150, 0]);
-
-    const colorScale = scaleLinear()
-      .domain([75, 100, 150])
-      .range(["green", "orange", "red"])
-      .clamp(true);
-
-    // create x-axis
-    const xAxis = axisBottom(xScale).ticks(data.length);
-    svg
-      .select(".x-axis")
-      .style("transform", "translateY(150px)")
-      .call(xAxis);
-
-    // create y-axis
-    const yAxis = axisRight(yScale);
-    svg
-      .select(".y-axis")
-      .style("transform", "translateX(300px)")
-      .call(yAxis);
-
-    // draw the bars
-    svg
-      .selectAll(".bar")
-      .data(data)
-      .join("rect")
-      .attr("class", "bar")
-      .style("transform", "scale(1, -1)")
-      .attr("x", (value, index) => xScale(index))
-      .attr("y", -150)
-      .attr("width", xScale.bandwidth())
-      .on("mouseenter", (value, index) => {
-        svg
-          .selectAll(".tooltip")
-          .data([value])
-          .join(enter => enter.append("text").attr("y", yScale(value) - 4))
-          .attr("class", "tooltip")
-          .text(value)
-          .attr("x", xScale(index) + xScale.bandwidth() / 2)
-          .attr("text-anchor", "middle")
-          .transition()
-          .attr("y", yScale(value) - 8)
-          .attr("opacity", 1);
-      })
-      .on("mouseleave", () => svg.select(".tooltip").remove())
-      .transition()
-      .attr("fill", colorScale)
-      .attr("height", value => 150 - yScale(value));
-  }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
+      <BarChart data={data} />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update data
       </button>
