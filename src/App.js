da5cf95a2047ccc92e01786bcec521427f5df6d2
Murat Kemaldar <murat.kemaldar@outlook.com>
2020-02-18 17:23:23 +0100

    simple pie chart

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import ml5 from "ml5";
-import GaugeChart from "./GaugeChart";
-import useInterval from "./useInterval";
-
-let classifier;
+import PieChart from "./PieChart";
 
 function App() {
-  const videoRef = useRef();
-  const [gaugeData, setGaugeData] = useState([0.5, 0.5]);
-  const [shouldClassify, setShouldClassify] = useState(false);
-
-  useEffect(() => {
-    classifier = ml5.imageClassifier("./my-model/model.json", () => {
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then(stream => {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-        });
-    });
-  }, []);
-
-  useInterval(() => {
-    if (classifier && shouldClassify) {
-      classifier.classify(videoRef.current, (error, results) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        results.sort((a, b) => b.label.localeCompare(a.label));
-        setGaugeData(results.map(entry => entry.confidence));
-      });
+  const [data, setData] = useState([
+    {
+      name: "apples",
+      value: 100
+    },
+    {
+      name: "bananas",
+      value: 300
+    },
+    {
+      name: "cherries",
+      value: 250
     }
-  }, 500);
+  ]);
 
   return (
     <React.Fragment>
-      <h1>
-        Is Muri there? <br />
-        <small>
-          [{gaugeData[0].toFixed(2)}, {gaugeData[1].toFixed(2)}]
-        </small>
-      </h1>
-      <GaugeChart data={gaugeData} />
-      <button onClick={() => setShouldClassify(!shouldClassify)}>
-        {shouldClassify ? "Stop classifying" : "Start classifying"}
+      <h1>Pie Chart</h1>
+      <PieChart data={data} />
+      <button
+        onClick={() => {
+          setData([
+            {
+              name: "apples",
+              value: Math.round(Math.random() * 100)
+            },
+            {
+              name: "bananas",
+              value: Math.round(Math.random() * 200)
+            },
+            {
+              name: "cherries",
+              value: Math.round(Math.random() * 300)
+            }
+          ]);
+        }}
+      >
+        Randomize
       </button>
-      <video
-        ref={videoRef}
-        style={{ transform: "scale(-1, 1)" }}
-        width="300"
-        height="150"
-      />
     </React.Fragment>
   );
 }
---
+++ src/PieChart.js
@@ -0,0 +1,62 @@
+import { arc, interpolate, pie, scaleOrdinal, schemeGreens, select } from "d3";
+import React, { useEffect, useRef } from "react";
+import useResizeObserver from "./useResizeObserver";
+
+function PieChart({ data }) {
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    if (!dimensions) return;
+
+    // arc takes instructions (objects with special properties, like startAngle, endAngle, etc.)
+    // and transforms them into "d" attributes for path elements
+    const arcGenerator = arc()
+      .innerRadius(0)
+      .outerRadius(150);
+
+    // pie will transform data to instructions for arcGenerator
+    const pieGenerator = pie()
+      .value(d => d.value)
+      .sort(null);
+
+    // now transform data to instructions for arc()
+    const instructions = pieGenerator(data);
+
+    // generate colorScale
+    // https://github.com/d3/d3-scale-chromatic
+    const colorScale = scaleOrdinal(schemeGreens[3]);
+
+    // render slices (instructions)
+    svg
+      .selectAll(".slice")
+      .data(instructions)
+      .join("path")
+      .attr("class", "slice")
+      .attr("fill", (d, i) => colorScale(i))
+      .style(
+        "transform",
+        `translate(${dimensions.width / 2}px, ${dimensions.height / 2}px)`
+      )
+      .transition()
+      .attrTween("d", function(nextInstruction, index) {
+        // animation when changing data
+        const interpolator = interpolate(this.lastInstruction, nextInstruction);
+        this.lastInstruction = interpolator(1);
+        return function(t) {
+          return arcGenerator(interpolator(t));
+        };
+      });
+  }, [data, dimensions]);
+
+  return (
+    <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+      <svg ref={svgRef}></svg>
+    </div>
+  );
+}
+
+export default PieChart;
